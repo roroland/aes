6a58f5cb6759f398fbb6b401f06ba684ecd12a9f
rodrigonoales <rodrigonoales@centraldereservas.com>
2022-02-12 11:19:53 -0300

    Improve script and animations

--- js/script.js
+++ js/script.js
@@ -11,51 +11,58 @@ charsArr.forEach(function (item, i) {
     activeCharacter.classList.remove('active');
     item.classList.add('active');
     characterImage.setAttribute('src', `/images/${item.getAttribute('id')}.png`);
-    console.log(characterImage);
     activeCharacter = item;
+    characterImage.style.animation = 'none';
+    characterImage.style.opacity = '0';
+    if (item.classList.contains('active')) {
+      setTimeout(
+        charSwitch
+        ), 3000;
+      }
+    })
   })
-})
-
-let uiAnimOptions = {
-  rootMargin: '0px',
-  threshold: 0
-}
-
-function animSet(itemTarget) {
-  itemTarget.style.animation = `${itemTarget.dataset.animtype} .75s ease-out ${itemTarget.dataset.delay} forwards`;
-}
-function animNoDelaySet(itemTarget) {
-  itemTarget.style.animation = `${itemTarget.dataset.animtype} .75s ease-in-out forwards`;
-}
-
-let uiAnim = new IntersectionObserver((entries) => {
   
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      console.log(entry.target + 'IN');
-      if(entry.target.dataset.delay === undefined){
+  let uiAnimOptions = {
+    rootMargin: '0px',
+    threshold: 0
+  }
+  function charSwitch() {
+    characterImage.style.animation = 'fadeIn 1s ease-out .25s forwards';
+  }
+  
+  function animSet(itemTarget) {
+    itemTarget.style.animation = `${itemTarget.dataset.animtype} .5s ease-out ${itemTarget.dataset.delay} forwards`;
+  }
+  function animNoDelaySet(itemTarget) {
+    itemTarget.style.animation = `${itemTarget.dataset.animtype} .5s ease-in-out forwards`;
+  }
+  
+  let uiAnim = new IntersectionObserver((entries) => {
+    
+    entries.forEach(entry => {
+      if(entry.isIntersecting && entry.target.dataset.delay === undefined){
         animNoDelaySet(entry.target);
         if(entry.target.dataset.once) {
           animNoDelaySet(entry.target);
           uiAnim.unobserve(entry.target);
         }
       } 
-      else {
+      else if(entry.isIntersecting) {
         animSet(entry.target);
-        if(entry.target.dataset.once) {
+        if(entry.isIntersecting && entry.target.dataset.once) {
           animSet(entry.target);
           uiAnim.unobserve(entry.target);
         }
+        return;
       } 
-    }
-    else {
-      entry.target.style.animation = 'none';
-      console.log(entry.target + 'OUT');
+      else {
+        entry.target.style.animation = 'none';
+        console.log(entry.target + 'OUT NEW');
+        return;
+      }
       
-      return;
-    }
-  })
-}, uiAnimOptions);
-animatable.forEach(itm => {
-  uiAnim.observe(itm);
-});
+    })
+  }, uiAnimOptions);
+  animatable.forEach(itm => {
+    uiAnim.observe(itm);
+  });
